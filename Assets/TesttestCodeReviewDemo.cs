cec0bea096f0ade3fd6a502f8a1309aace9d9f64
RSGameDev <rsgaming@hotmail.co.uk>
2021-05-31 17:31:59 +0100

    Showing code review feature

---
+++ Assets/TesttestCodeReviewDemo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesttestCodeReviewDemo : MonoBehaviour
+{
+    //hello , this is a demo for code review feature within Github
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
