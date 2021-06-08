fa7b7be5745a61006721be0c4fabad3d6408194d
RSGameDev <rsgaming@hotmail.co.uk>
2021-06-08 16:43:15 +0100

    Kettle Implementation
    The kettle now explodes after a period of time.
    The player once within a radius activates a timer.
    Kettle explodes once timer reached.
    If player leaves the radius the timer resets.

--- Assets/Scripts/Hazards/Kettle.cs
+++ Assets/Scripts/Hazards/Kettle.cs
@@ -6,12 +6,37 @@ namespace Hazards
 {
     public class Kettle : Hazard
     {
+        private bool _isPlayerDetected;
+        [SerializeField] private float _timer;
+        [SerializeField] private float _timeOfExplosion;
+
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.gameObject.GetComponent<Health>())
+            if (other.gameObject.CompareTag("Player"))
             {
-                Damage(damageValue);
-                Destroy(gameObject);
+                _isPlayerDetected = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                _isPlayerDetected = false;
+                _timer = 0f;
+            }
+        }
+
+        private void Update()
+        {
+            if (_isPlayerDetected)
+            {
+                _timer += Time.deltaTime;
+                if (_timer >= _timeOfExplosion)
+                {
+                    Damage(damageValue);
+                    Destroy(gameObject);
+                }
             }
         }
     }
