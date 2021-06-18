7a5f82282df18e7f6fa0f5bc78fbc623d32c6b32
AJ Williams <aj.williams.up@gmail.com>
2021-06-18 12:51:15 +0100

    Added Script Creation

--- Assets/Scripts/Dialog/DialogScript.cs
+++ Assets/Scripts/Dialog/DialogScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "ScriptData")]
 public class DialogScript : ScriptableObject
 {
     List<string> script;
