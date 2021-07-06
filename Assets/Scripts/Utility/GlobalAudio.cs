5b0715a561d4b2ec052bd2addae529673643c3ac
Akokko200 <45992576+Akokko200@users.noreply.github.com>
2021-07-06 11:11:58 +0100

    Update GlobalAudio.cs

--- Assets/Scripts/Utility/GlobalAudio.cs
+++ Assets/Scripts/Utility/GlobalAudio.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class GlobalAudio : MonoBehaviour
 {
+    //These variables exist across all instances of global audio, changing them updates the aduiosource on the gameobject accordingly.
     public static float volume = 1;
     public static bool mute = false;
+
+   
     private AudioSource audioComponent;
+
+    //Instead of changing the volume directly, we're taking a starting volume and modifying it according to the global volume.
+    //This allows control over audio, while not sacrificing volume depth within the scene
+    //Aka, music can be quiet and jump can be at full volume, but if global volume is set to .4f they both scale accordingly.
     private float startingVolume;
     void Start()
     {
@@ -17,6 +24,7 @@ public class GlobalAudio : MonoBehaviour
    
     void Update()
     {
+        //By calling for a change only if the values don't match up we're making sure not to waste any memory. 
         if (startingVolume*volume!=audioComponent.volume)
         {
             audioComponent.volume = startingVolume * volume;
